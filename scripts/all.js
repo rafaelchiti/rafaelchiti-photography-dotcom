7e1d1f5a263f28e1dab1a79aec991162a34631e1
Rafael Chiti <rafaelchiti@gmail.com>
2014-08-04 12:58:18 +0200

    Photo expo impr Adding jquery UI Improving the photo expo features

--- scripts/all.js
+++ scripts/all.js
@@ -7,6 +7,10 @@ var onScroll = _.throttle(function() {
   } else {
     $('body').removeClass('is-page-scrolled');
   }
+
+  if ($('body').hasClass('is-exposing-photo')) {
+    $('.js-backgroundBlocker').addClass('has-scrolled-while-exposing');
+  }
 }, 300, this);
 
 $(window).scroll(onScroll);
@@ -22,7 +26,7 @@ $(document).ready(function() {
 
   configurePhotoExpo();
 
-  $('.js-everyday-chiswick').click();
+  $('.js-a-particular-pick').click();
 });
 
 function configureToolbar() {
@@ -57,7 +61,7 @@ function configureEverydayChiswick() {
 }
 
 function configureAParticularPick() {
-  configureImages('a_particular_pick', 28);
+  configureImages('a_particular_pick', 26);
 }
 
 function configureImages(projectFolderName, photosCount) {
@@ -100,32 +104,66 @@ function loadEveryDayChiswick() {
 
 
 function configurePhotoExpo() {
+  var $photoExpo = $('.js-photoExpo');
+  var $photoExpoWrapper = $('.js-photoExpoWrapper');
+  var $photoExpoImg = $('.js-photoExpo img');
+  var $backgroundBlocker = $('.js-backgroundBlocker');
+
   $('body').on('click', '.js-photoSlider .js-photoWrapper', function(event) {
 
     $('.js-backgroundBlocker').show();
 
+    $('body').addClass('is-exposing-photo');
+
     var imgLoaded = imagesLoaded('.js-photoExpo');
 
     imgLoaded.on('done', function(instance, image) {
-      $('.js-photoExpo').removeClass('is-loading');
+      $photoExpo.removeClass('is-loading');
       imgLoaded.off();
+
+      setPhotoExpoSizeClass($photoExpoImg, $photoExpo);
     });
 
+    $photoExpoWrapper.css('top', $(document).scrollTop() + 30);
 
-    $('.js-photoExpo').fadeIn();
-    $('.js-photoExpo').addClass('is-loading');
+    $photoExpo.addClass('is-loading');
+    $photoExpoWrapper.fadeIn();
 
     var src = $(event.currentTarget).children('img').attr('src');
 
-    $('.js-photoExpo img').attr('src', src);
+    $photoExpoImg.attr('src', src);
+
 
   });
 
-  $('.js-backgroundBlocker').click(function(event) {
-    $(event.currentTarget).hide();
-    $('.js-photoExpo').hide();
-  });
+  function hidePhotoExpo() {
+    $backgroundBlocker.hide();
+    $photoExpoWrapper.hide();
+    $('body').removeClass('is-exposing-photo');
+    $backgroundBlocker.removeClass('has-scrolled-while-exposing');
+  }
 
+  $photoExpoWrapper.click(hidePhotoExpo);
+  $backgroundBlocker.click(hidePhotoExpo);
+
+}
+
+function setPhotoExpoSizeClass($photoExpoImg, $photoExpo) {
+  var width = $photoExpoImg.width();
+  var height = $photoExpoImg.height();
+
+  var delta = height - width;
+
+  console.log('calc:', height / 2);
+  console.log('delta', delta);
+
+  if (width > height) {
+    $photoExpo.switchClass('is-tall is-really-tall', 'is-wider', 0);
+  } else if (height / 2 > delta && width !== height) {
+    $photoExpo.switchClass('is-tall is-wider', 'is-really-tall', 0);
+  } else {
+    $photoExpo.switchClass('is-really-tall is-wider', 'is-tall', 0);
+  }
 }
 
 
